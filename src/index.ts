cbaf75531c45268f0abf474df426ea566fc545de
RJ <aldeareneyao@gmail.com>
2024-08-22 21:41:27 +0800

    export Spinner, Illustration, DataListTable components to index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// Common
+export { default as MUIButton } from "./components/Common/MUIButton";
+
+// Containers
+
+// InputFields
+
+// Tables
+export { default as DataListTable } from "./components/Tables/DataListTable";
+
+// Utilities
+export { default as Spinner } from "./components/Utilities/Spinner";
+export { default as Illustration } from "./components/Utilities/Illustration";
