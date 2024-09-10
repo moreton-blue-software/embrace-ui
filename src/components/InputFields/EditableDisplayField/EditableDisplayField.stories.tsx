67c728a5843e4b905b1e1b7f385bb728312a2802
RJ <aldeareneyao@gmail.com>
2024-09-10 09:37:08 +0800

    #3 added code source for EditableDisplayField story docs

--- src/components/InputFields/EditableDisplayField/EditableDisplayField.stories.tsx
+++ src/components/InputFields/EditableDisplayField/EditableDisplayField.stories.tsx
@@ -1,15 +1,17 @@
 import { useForm } from "react-hook-form";
 
 import type { Meta, StoryObj } from "@storybook/react";
+import { Title, Story, Source } from "@storybook/blocks";
 
 import EditableDisplayField from ".";
 import { Box, TextField } from "@mui/material";
+
+import * as CODE from "./EditableDisplayField.source";
 
 const meta: Meta<typeof EditableDisplayField> = {
   title: "Components/Input Fields/Editable Display Field",
   tags: ["autodocs"],
   component: EditableDisplayField,
-  parameters: { layout: "centered" },
   decorators: [
     (Story) => (
       <Box sx={{ bgcolor: "#F5F5F5", borderRadius: 4, px: 8, py: 4 }}>
@@ -17,6 +19,46 @@ const meta: Meta<typeof EditableDisplayField> = {
       </Box>
     ),
   ],
+  parameters: {
+    layout: "centered",
+    docs: {
+      page: () => (
+        <>
+          <Title>Edit Mode</Title>
+          <Story of={EditMode} />
+          <Source of={EditMode} code={CODE.EDIT_MODE_SOURCE} />
+
+          <Title>Display Mode with Default</Title>
+          <Story of={DisplayModeWithDefault} />
+          <Source
+            of={DisplayModeWithDefault}
+            code={CODE.DISPLAY_MODE_WITH_DEFAULT_SOURCE}
+          />
+
+          <Title>Display Mode with Default Date</Title>
+          <Story of={DisplayModeWithDefaultDate} />
+          <Source
+            of={DisplayModeWithDefaultDate}
+            code={CODE.DISPLAY_MODE_WITH_DEFAULT_DATE_SOURCE}
+          />
+
+          <Title>Edit Mode with Default</Title>
+          <Story of={EditModeWithDefault} />
+          <Source
+            of={EditModeWithDefault}
+            code={CODE.EDIT_MODE_WITH_DEFAULT_SOURCE}
+          />
+
+          <Title>Full Width Edit Field</Title>
+          <Story of={FullWidthEditField} />
+          <Source
+            of={FullWidthEditField}
+            code={CODE.FULL_WIDTH_EDIT_FIELD_SOURCE}
+          />
+        </>
+      ),
+    },
+  },
 };
 
 export default meta;
@@ -28,6 +70,9 @@ export const EditMode: Story = {
     title: "Sample Title",
     name: "sampleName",
     isEdit: true,
+  },
+  parameters: {
+    docs: { source: { code: `this is the code` } },
   },
   render: (args) => {
     const form = useForm();
